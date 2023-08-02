537a78da1c63027665cd748e9ca7d79472599afd
Anton Bakker <anton.bakker@kadaster.nl>
2023-08-02 11:16:04 +0200

    add api version header to responses

--- coordinates_transformation_api/main.py
+++ coordinates_transformation_api/main.py
@@ -1,14 +1,13 @@
 from importlib import resources as impresources
 from typing import List
+import time
 
 import uvicorn
 import yaml
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
 from . import assets
-
-default_headers = {"API-Version": "2.0.1"}
 
 app = FastAPI(docs_url="/api")
 
@@ -30,8 +29,11 @@ class Conformance(BaseModel):
     conformsTo: List[str] = []
 
 
-# class VndResponse(JSONResponse):
-#     media_type = 'application/vnd.oai.openapi+json;version=3.0'
+@app.middleware("http")
+async def add_api_version(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["API-Version"] = API_VERSION
+    return response
 
 
 @app.get("/", response_model=LandingPage)
@@ -84,13 +86,12 @@ async def transform():
     ]
 
 
-def custom_openapi():
-    oas_file_resource = impresources.files(assets) / "openapi.yaml"
-    with oas_file_resource.open("rb") as oas_file:
-        return yaml.load(oas_file, yaml.SafeLoader)
+OAS_FILEPATH = impresources.files(assets) / "openapi.yaml"
 
-
-app.openapi = custom_openapi
+with OAS_FILEPATH.open("rb") as oas_file:
+    OPEN_API_SPEC = yaml.load(oas_file, yaml.SafeLoader)
+API_VERSION = OPEN_API_SPEC["info"]["version"]
+app.openapi = OPEN_API_SPEC
 
 
 def start():
