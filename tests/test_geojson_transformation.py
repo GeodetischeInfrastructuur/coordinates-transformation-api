cfdddf130c3fcfd0ae1c17cd1a99d9381dd53817
Anton Bakker <anton.bakker@kadaster.nl>
2023-11-15 12:34:46 +0100

    add cors headers settings

--- tests/test_geojson_transformation.py
+++ tests/test_geojson_transformation.py
@@ -1,14 +1,12 @@
 import json
 
+from coordinates_transformation_api.models import CrsFeatureCollection
+from coordinates_transformation_api.util import crs_transform
 from geojson_pydantic import Feature
 from geojson_pydantic.geometries import Geometry, GeometryCollection, parse_geometry_obj
 from pydantic import ValidationError
 
-from coordinates_transformation_api.models import CrsFeatureCollection
-from coordinates_transformation_api.util import crs_transform, init_oas
 from tests.util import not_raises
-
-_, _, _ = init_oas()
 
 
 def test_bbox_transformed():
