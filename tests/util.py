eb8253d463335caf4275bd3147b7a02731eb4261
Anton Bakker <anton.bakker@kadaster.nl>
2023-12-08 11:20:10 +0100

    add linting and tests workflow + update precommit hook

--- tests/util.py
+++ tests/util.py
@@ -9,7 +9,7 @@ def not_raises(exception, message: Optional[str] = ""):
     try:
         yield
     except exception:
-        if message !="":
+        if message != "":
             raise pytest.fail(f"DID RAISE {exception}")  # noqa: B904
         else:
             raise pytest.fail(message.format(exc=exception))  # noqa: B904
