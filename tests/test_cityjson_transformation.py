b39bd022fc0757c7a8c00a8c4dee8222d3ec6af0
Wouter <wouter.visscher@gmail.com>
2023-12-19 15:45:01 +0100

    updated transformer selection (#73)
    * updated transformer selection
    
    * updated crs list
    
    * black formatting
    
    * added step download NL corr grid tif
    
    * update pyproj prpoj_data dir
    
    * added all nl_nsgi_*.tif files
    
    * impl source & target crs filtering
    
    * update raise_ func
    
    * update error msg
    
    * fix pre-commit issues
    
    * black formatting

--- tests/test_cityjson_transformation.py
+++ tests/test_cityjson_transformation.py
@@ -9,11 +9,11 @@ def test_cityjson_transformed():
         cj = CityjsonV113.model_validate(data)
         cj_original = CityjsonV113.model_validate(data)
 
-        cj.crs_transform("EPSG:7415", "EPSG:9928")
+        cj.crs_transform("EPSG:7415", "EPSG:9753")
         assert cj.metadata.geographicalExtent != cj_original.metadata.geographicalExtent
         assert cj.vertices != cj_original.vertices
         assert (
-            cj.metadata.referenceSystem == "https://www.opengis.net/def/crs/EPSG/0/9928"
+            cj.metadata.referenceSystem == "https://www.opengis.net/def/crs/EPSG/0/9753"
         )
         assert cj.transform is not None
         assert cj.transform != cj_original.transform
