adb99ea19d428c8a9dba8cf9ac0050489df8f5eb
Wouter <wouter.visscher@gmail.com>
2024-02-08 09:06:39 +0100

    larger bbox constant
    Co-authored-by: Jochem-L <Jochem.Lesparre@kadaster.nl>

--- src/coordinate_transformation_api/constants.py
+++ src/coordinate_transformation_api/constants.py
@@ -5,11 +5,11 @@ HEIGHT_DIGITS_FOR_ROUNDING = (
 DENSIFY_CRS_2D = "EPSG:9067"
 DENSIFY_CRS_3D = "EPSG:7931"
 DEVIATION_VALID_BBOX = [
-    3.1201,
-    50.2191,
-    7.5696,
-    54.1238,
-]  # bbox in epsg:4258 - area valid for doing density check (based on deviation param)
+    2.0,
+    50.0,
+    8.0,
+    56.0,
+]  # bbox of RD and NAP GeoTIFF grids in epsg:9067 - area valid for doing density check (based on deviation param)
 DENSITY_CHECK_RESULT_HEADER = "density-check-result"
 THREE_DIMENSIONAL = 3
 TWO_DIMENSIONAL = 2
