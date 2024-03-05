d4a4b3cbe20d7a0fbca0d009c935b1b799b124f5
Wouter <wouter.visscher@gmail.com>
2024-03-05 14:28:59 +0100

    2d epoch (#145)
    * Bump the python-packages group with 6 updates
    
    Bumps the python-packages group with 6 updates:
    
    | Package | From | To |
    | --- | --- | --- |
    | [fastapi[all]](https://github.com/tiangolo/fastapi) | `0.109.2` | `0.110.0` |
    | [pydantic-settings](https://github.com/pydantic/pydantic-settings) | `2.1.0` | `2.2.1` |
    | [email-validator](https://github.com/JoshData/python-email-validator) | `2.1.0.post1` | `2.1.1` |
    | [coverage](https://github.com/nedbat/coveragepy) | `7.4.1` | `7.4.3` |
    | [pytest](https://github.com/pytest-dev/pytest) | `8.0.0` | `8.1.0` |
    | [ruff](https://github.com/astral-sh/ruff) | `0.2.1` | `0.3.0` |
    
    
    Updates `fastapi[all]` from 0.109.2 to 0.110.0
    - [Release notes](https://github.com/tiangolo/fastapi/releases)
    - [Commits](https://github.com/tiangolo/fastapi/compare/0.109.2...0.110.0)
    
    Updates `pydantic-settings` from 2.1.0 to 2.2.1
    - [Release notes](https://github.com/pydantic/pydantic-settings/releases)
    - [Commits](https://github.com/pydantic/pydantic-settings/compare/v2.1.0...v2.2.1)
    
    Updates `email-validator` from 2.1.0.post1 to 2.1.1
    - [Release notes](https://github.com/JoshData/python-email-validator/releases)
    - [Changelog](https://github.com/JoshData/python-email-validator/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/JoshData/python-email-validator/commits/v2.1.1)
    
    Updates `coverage` from 7.4.1 to 7.4.3
    - [Release notes](https://github.com/nedbat/coveragepy/releases)
    - [Changelog](https://github.com/nedbat/coveragepy/blob/master/CHANGES.rst)
    - [Commits](https://github.com/nedbat/coveragepy/compare/7.4.1...7.4.3)
    
    Updates `pytest` from 8.0.0 to 8.1.0
    - [Release notes](https://github.com/pytest-dev/pytest/releases)
    - [Changelog](https://github.com/pytest-dev/pytest/blob/main/CHANGELOG.rst)
    - [Commits](https://github.com/pytest-dev/pytest/compare/8.0.0...8.1.0)
    
    Updates `ruff` from 0.2.1 to 0.3.0
    - [Release notes](https://github.com/astral-sh/ruff/releases)
    - [Changelog](https://github.com/astral-sh/ruff/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/astral-sh/ruff/compare/v0.2.1...v0.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: fastapi[all]
      dependency-type: direct:production
      update-type: version-update:semver-minor
      dependency-group: python-packages
    - dependency-name: pydantic-settings
      dependency-type: direct:production
      update-type: version-update:semver-minor
      dependency-group: python-packages
    - dependency-name: email-validator
      dependency-type: direct:production
      update-type: version-update:semver-patch
      dependency-group: python-packages
    - dependency-name: coverage
      dependency-type: direct:production
      update-type: version-update:semver-patch
      dependency-group: python-packages
    - dependency-name: pytest
      dependency-type: direct:production
      update-type: version-update:semver-minor
      dependency-group: python-packages
    - dependency-name: ruff
      dependency-type: direct:production
      update-type: version-update:semver-minor
      dependency-group: python-packages
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * added 2d with epoch test
    
    * mv logic -> own def, added & update tests
    
    * fix launch.json
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- tests/test_transformer_selection.py
+++ tests/test_transformer_selection.py
@@ -1,5 +1,9 @@
 import pytest
-from coordinate_transformation_api.crs_transform import get_transformer, needs_epoch
+from coordinate_transformation_api.crs_transform import (
+    build_input_coord,
+    get_transformer,
+    needs_epoch,
+)
 
 
 # This test needs the modified proj.time.dependent.transformations.db from
@@ -17,3 +21,16 @@ from coordinate_transformation_api.crs_transform import get_transformer, needs_e
 )
 def test_time_dependant_operation_method(source, target, epoch, expectation):
     assert needs_epoch(get_transformer(source, target, epoch)) == expectation
+
+
+@pytest.mark.parametrize(
+    ("coord", "epoch", "expectation"),
+    [
+        (tuple([1000.0, 1000.0]), 2000.0, tuple([1000.0, 1000.0, 0.0, 2000.0])),
+        (tuple([1000.0, 1000.0]), None, tuple([1000.0, 1000.0, None])),
+        (tuple([1000.0, 1000.0, 10.0]), 2000.0, tuple([1000.0, 1000.0, 10.0, 2000.0])),
+        (tuple([1000.0, 1000.0, 10.0]), None, tuple([1000.0, 1000.0, 10.0, None])),
+    ],
+)
+def test_build_input_coord(coord, epoch, expectation):
+    assert build_input_coord(coord, epoch) == expectation
