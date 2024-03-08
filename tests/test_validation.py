92ba3c8641f8f93230420c3c4dbb72d8108e24ea
Wouter Visscher <wouter.visscher@gmail.com>
2024-03-08 12:39:40 +0100

    added 'caching' for validation test

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,68 +1,14 @@
 import pytest
 from coordinate_transformation_api.crs_transform import get_transform_crs_fun
 from coordinate_transformation_api.models import Crs as MyCrs
-from pyproj import transformer
+from coordinate_transformation_api.models import TransformationNotPossibleError
 
-seed_crs_list = [
-    "EPSG:7415",
-    "EPSG:28992",
-    "EPSG:4258",
-    "EPSG:3035",
-    "EPSG:3034",
-    "EPSG:3043",
-    "EPSG:3044",
-    "EPSG:9067",
-    "OGC:CRS84",
-    "EPSG:4326",
-    "EPSG:3857",
-    "EPSG:32631",
-    "EPSG:32632",
-    "EPSG:9000",
-    "EPSG:9755",
-    "EPSG:4937",
-    "EPSG:4936",
-    "EPSG:9286",
-    "EPSG:7931",
-    "EPSG:7930",
-    "OGC:CRS84h",
-    "EPSG:4979",
-    "EPSG:7912",
-    "EPSG:7789",
-    "EPSG:9754",
-    "EPSG:9753",
-]
-seed_init_crs = "EPSG:7415"
-seed_coord = (100000, 300000, 43, 2000)
+from tests.util import do_pyproj_transformation, validation_data
 
 
-def do_pyproj_transformation(
-    source_crs: str, target_crs: str, coords: tuple[float, ...]
-) -> tuple[float, ...]:
-    tfg = transformer.TransformerGroup(
-        source_crs, target_crs, allow_ballpark=False, always_xy=True
-    )
-
-    if len(tfg.transformers) == 0:
-        return (float("inf"), float("inf"), float("inf"), float("inf"))
-
-    return tfg.transformers[0].transform(*coords)
-
-
-def get_test_data() -> tuple[str, str, tuple[float, ...]]:
-
-    test_data = []
-
-    for source_crs in seed_crs_list:
-        for target_crs in seed_crs_list:
-            source_coord = do_pyproj_transformation(
-                seed_init_crs, target_crs, seed_coord
-            )
-            test_data.append((source_crs, target_crs, source_coord))
-
-    return test_data
-
-
-@pytest.mark.parametrize("source_crs", "target_crs", "source_coord", get_test_data())
+@pytest.mark.parametrize(
+    ("source_crs", "target_crs", "source_coord"), validation_data()
+)
 def test_transformation(source_crs, target_crs, source_coord):
 
     source_crs_info = MyCrs.from_crs_str(source_crs)
@@ -70,9 +16,12 @@ def test_transformation(source_crs, target_crs, source_coord):
     unit = target_crs_info.get_x_unit_crs()
 
     if source_crs_info.nr_of_dimensions < target_crs_info.nr_of_dimensions:
-        with pytest.raises(Exception, match="ValueError") as e:
+        with pytest.raises(
+            TransformationNotPossibleError,
+            match="number of dimensions source-crs: 2, number of dimensions target-crs: 3",
+        ) as e:
             get_transform_crs_fun(source_crs, target_crs)(source_coord)
-        assert type(e.value.__cause__) is ValueError
+        assert type(e.value) is TransformationNotPossibleError
     else:
         api_transformed_coord = get_transform_crs_fun(
             source_crs,
--- tests/util.py
+++ tests/util.py
@@ -1,7 +1,10 @@
+import csv
+import os
 from contextlib import contextmanager
 from typing import Optional
 
 import pytest
+from pyproj import transformer
 
 
 @contextmanager
@@ -13,3 +16,84 @@ def not_raises(exception, message: Optional[str] = ""):
             raise pytest.fail(f"DID RAISE {exception}")  # noqa: B904
         else:
             raise pytest.fail(message.format(exc=exception))  # noqa: B904
+
+
+def do_pyproj_transformation(
+    source_crs: str, target_crs: str, coords: tuple[float, ...]
+) -> tuple[float, ...]:
+    tfg = transformer.TransformerGroup(
+        source_crs, target_crs, allow_ballpark=False, always_xy=True
+    )
+
+    if len(tfg.transformers) == 0:
+        return (float("inf"), float("inf"), float("inf"), float("inf"))
+
+    return tfg.transformers[0].transform(*coords)
+
+
+def make_entry(line):
+    source = line[0]
+    target = line[1]
+    coords = tuple(float(num) for num in line[2].strip("()").split(" "))
+    return tuple([source, target, coords])
+
+
+def validation_data():
+
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+
+    seed_crs_list = [
+        "EPSG:7415",
+        "EPSG:28992",
+        "EPSG:4258",
+        "EPSG:3035",
+        "EPSG:3034",
+        "EPSG:3043",
+        "EPSG:3044",
+        "EPSG:9067",
+        "OGC:CRS84",
+        "EPSG:4326",
+        "EPSG:3857",
+        "EPSG:32631",
+        "EPSG:32632",
+        "EPSG:9000",
+        "EPSG:9755",
+        "EPSG:4937",
+        "EPSG:4936",
+        "EPSG:9286",
+        "EPSG:7931",
+        "EPSG:7930",
+        "OGC:CRS84h",
+        "EPSG:4979",
+        "EPSG:7912",
+        "EPSG:7789",
+        "EPSG:9754",
+        "EPSG:9753",
+    ]
+    seed_init_crs = "EPSG:7415"
+    seed_coord = (100000, 300000, 43, 2000)
+
+    file = os.path.join(test_dir, "data", "validation_data.csv")
+
+    if os.path.isfile(file) is True and os.stat(file).st_size != 0:
+        with open(os.path.join(test_dir, "data", "validation_data.csv")) as fread:
+            t = [make_entry(line) for line in csv.reader(fread, delimiter=",")]
+            return t
+    else:
+        with open(os.path.join(test_dir, "data", "validation_data.csv"), "w") as fwrite:
+            for source_crs in seed_crs_list:
+                for target_crs in seed_crs_list:
+                    source_coord = do_pyproj_transformation(
+                        seed_init_crs, source_crs, seed_coord
+                    )
+                    fwrite.write(
+                        "{},{},{}\n".format(
+                            source_crs,
+                            target_crs,
+                            "({} {} {} {})".format(*source_coord),
+                        )
+                    )
+                    # fwrite.write('\n'.join(','.join((source_crs, target_crs))))
+                    # json.dump(json.dumps(tuple[source_crs, target_crs, source_coord]), fwrite)
+
+        validation_data()
