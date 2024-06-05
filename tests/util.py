25d09f914bbed522e7d7e345687dedadefe1d656
Wouter <wouter.visscher@gmail.com>
2024-06-05 10:57:36 +0200

    added init BES validation (#185)
    * added init BES validation
    
    * added international crs for BES
    
    * renamed test files
    
    * rm test/tmp file

--- tests/util.py
+++ tests/util.py
@@ -6,6 +6,8 @@ from typing import Optional
 import pytest
 from pyproj import transformer
 
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 @contextmanager
 def not_raises(exception, message: Optional[str] = ""):
@@ -38,9 +40,7 @@ def make_entry(line):
     return tuple([source, target, coords])
 
 
-def validation_data():
-
-    test_dir = os.path.dirname(os.path.abspath(__file__))
+def nl_eu_validation_data():
 
     seed_crs_list = [
         "EPSG:7415",
@@ -73,14 +73,14 @@ def validation_data():
     seed_init_crs = "EPSG:7415"
     seed_coord = (0, 400000, 43, 2000)
 
-    file = os.path.join(test_dir, "data", "validation_data.csv")
+    file = os.path.join(TEST_DIR, "data", "nl_validation_data.csv")
 
     if os.path.isfile(file) is True and os.stat(file).st_size != 0:
-        with open(os.path.join(test_dir, "data", "validation_data.csv")) as fread:
+        with open(file) as fread:
             t = [make_entry(line) for line in csv.reader(fread, delimiter=",")]
             return t
     else:
-        with open(os.path.join(test_dir, "data", "validation_data.csv"), "w") as fwrite:
+        with open(file, "w") as fwrite:
             for source_crs in seed_crs_list:
                 for target_crs in seed_crs_list:
                     source_coord = do_pyproj_transformation(
@@ -94,4 +94,125 @@ def validation_data():
                         )
                     )
 
-        validation_data()
+        nl_eu_validation_data()
+
+
+def nl_bonaire_validation_data():
+
+    seed_crs_list = [
+        "NSGI:Bonaire_DPnet_KADpeil",
+        "NSGI:Bonaire_DPnet",
+        "NSGI:Bonaire2004_GEOCENTRIC",
+        "NSGI:Bonaire2004_GEOGRAPHIC_2D",
+        "NSGI:Bonaire2004_GEOGRAPHIC_3D",
+        "EPSG:32619",
+        "EPSG:7789EPSG:7912",
+        "EPSG:4979",
+        "OGC:CRS84h",
+    ]
+    seed_init_crs = "NSGI:Bonaire_DPnet_KADpeil"
+    seed_coord = (23000.0000, 18000.0000, 10.0000, 2000)
+
+    file = os.path.join(TEST_DIR, "data", "bonaire_validation_data.csv")
+
+    if os.path.isfile(file) is True and os.stat(file).st_size != 0:
+        with open(file) as fread:
+            t = [make_entry(line) for line in csv.reader(fread, delimiter=",")]
+            return t
+    else:
+        with open(file, "w") as fwrite:
+            for source_crs in seed_crs_list:
+                for target_crs in seed_crs_list:
+                    source_coord = do_pyproj_transformation(
+                        seed_init_crs, source_crs, seed_coord
+                    )
+                    fwrite.write(
+                        "{},{},{}\n".format(
+                            source_crs,
+                            target_crs,
+                            "({} {} {} {})".format(*source_coord),
+                        )
+                    )
+
+        nl_bonaire_validation_data()
+
+
+def nl_st_eustatius_validation_data():
+
+    seed_crs_list = [
+        "NSGI:St_Eustatius_DPnet_Height",
+        "NSGI:St_Eustatius_DPnet",
+        "NSGI:St_Eustatius2020_GEOCENTRIC",
+        "NSGI:St_Eustatius2020_GEOGRAPHIC_2D",
+        "NSGI:St_Eustatius2020_GEOGRAPHIC_3D",
+        "EPSG:32619",
+        "EPSG:7789EPSG:7912",
+        "EPSG:4979",
+        "OGC:CRS84h",
+    ]
+    seed_init_crs = "NSGI:St_Eustatius_DPnet_Height"
+    seed_coord = (502000.0000, 1934000.0000, 100.0000, 2000)
+
+    file = os.path.join(TEST_DIR, "data", "st_eustatius_validation_data.csv")
+
+    if os.path.isfile(file) is True and os.stat(file).st_size != 0:
+        with open(file) as fread:
+            t = [make_entry(line) for line in csv.reader(fread, delimiter=",")]
+            return t
+    else:
+        with open(file, "w") as fwrite:
+            for source_crs in seed_crs_list:
+                for target_crs in seed_crs_list:
+                    source_coord = do_pyproj_transformation(
+                        seed_init_crs, source_crs, seed_coord
+                    )
+                    fwrite.write(
+                        "{},{},{}\n".format(
+                            source_crs,
+                            target_crs,
+                            "({} {} {} {})".format(*source_coord),
+                        )
+                    )
+
+        nl_st_eustatius_validation_data()
+
+
+def nl_saba_validation_data():
+
+    seed_crs_list = [
+        "NSGI:Saba_DPnet_Height",
+        "NSGI:Saba_DPnet",
+        "NSGI:Saba_DPnet_Height",
+        "NSGI:Saba2020_GEOCENTRIC",
+        "NSGI:Saba2020_GEOGRAPHIC_2D",
+        "NSGI:Saba2020_GEOGRAPHIC_3D",
+        "EPSG:32619",
+        "EPSG:7789EPSG:7912",
+        "EPSG:4979",
+        "OGC:CRS84h",
+    ]
+    seed_init_crs = "NSGI:Saba_DPnet_Height"
+    seed_coord = (5000.0000, 1000.0000, 300.0000, 2000)
+
+    file = os.path.join(TEST_DIR, "data", "saba_validation_data.csv")
+
+    if os.path.isfile(file) is True and os.stat(file).st_size != 0:
+        with open(file) as fread:
+            t = [make_entry(line) for line in csv.reader(fread, delimiter=",")]
+            return t
+    else:
+        with open(file, "w") as fwrite:
+            for source_crs in seed_crs_list:
+                for target_crs in seed_crs_list:
+                    source_coord = do_pyproj_transformation(
+                        seed_init_crs, source_crs, seed_coord
+                    )
+                    fwrite.write(
+                        "{},{},{}\n".format(
+                            source_crs,
+                            target_crs,
+                            "({} {} {} {})".format(*source_coord),
+                        )
+                    )
+
+        nl_saba_validation_data()
