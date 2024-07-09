8353da58223b299cef46e15d3abbd58d7bab54ad
Wouter <wouter.visscher@gmail.com>
2024-07-09 14:38:20 +0200

    rm get_geojson_obj -> textio_to_geojson

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import os
 
 import pytest
-from geodense.lib import get_geojson_obj
+from geodense.lib import textio_to_geojson
 
 
 @pytest.fixture()
@@ -12,13 +12,13 @@ def test_dir():
 @pytest.fixture()
 def geometry_collection_bbox(test_dir):
     with open(os.path.join(test_dir, "data", "geometry-collection-bbox.json")) as f:
-        return get_geojson_obj(f)
+        return textio_to_geojson(f)
 
 
 @pytest.fixture()
 def feature(test_dir):
     with open(os.path.join(test_dir, "data", "feature.json")) as f:
-        return get_geojson_obj(f)
+        return textio_to_geojson(f)
 
 
 @pytest.fixture()
@@ -26,22 +26,22 @@ def feature_collection_geometry_collection(test_dir):
     with open(
         os.path.join(test_dir, "data", "feature-collection-geometry-collection.json")
     ) as f:
-        return get_geojson_obj(f)
+        return textio_to_geojson(f)
 
 
 @pytest.fixture()
 def feature_geometry_collection(test_dir):
     with open(os.path.join(test_dir, "data", "feature-geometry-collection.json")) as f:
-        return get_geojson_obj(f)
+        return textio_to_geojson(f)
 
 
 @pytest.fixture()
 def polygons(test_dir):
     with open(os.path.join(test_dir, "data", "polygons.json")) as f:
-        return get_geojson_obj(f)
+        return textio_to_geojson(f)
 
 
 @pytest.fixture()
 def points(test_dir):
     with open(os.path.join(test_dir, "data", "points.json")) as f:
-        return get_geojson_obj(f)
+        return textio_to_geojson(f)
