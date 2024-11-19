9624758e4570d62a16348937dd776f5cf5584d21
Anton Bakker <anton.bakker@kadaster.nl>
2024-11-19 20:13:19 +0100

    use CRS.to_2d() for transforming with compound CRS's

--- tests/test_cityjson_transformation.py
+++ tests/test_cityjson_transformation.py
@@ -10,9 +10,9 @@ def test_cityjson_transformed():
         cj = CityjsonV113.model_validate(data)
         cj_original = CityjsonV113.model_validate(data)
 
-        cj.crs_transform(str_to_crs("EPSG:7415"), str_to_crs("EPSG:9753"), 2010.0)
+        cj.crs_transform(str_to_crs("EPSG:7415"), str_to_crs("EPSG:7931"), 2010.0)
         assert cj.metadata.geographicalExtent != cj_original.metadata.geographicalExtent
         assert cj.vertices != cj_original.vertices
-        assert cj.metadata.referenceSystem == "https://www.opengis.net/def/crs/EPSG/0/9753"
+        assert cj.metadata.referenceSystem == "https://www.opengis.net/def/crs/EPSG/0/7931"
         assert cj.transform is not None
         assert cj.transform != cj_original.transform
